b938db1bc76337b247a43f82cb9f5357e7949c50
Trevor Hartman <trevorhartman@gmail.com>
2024-10-07 07:50:52 -0600

    Add repro

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import "~/styles/globals.css";
 import type { AppProps } from "next/app";
+import { AppCacheProvider } from "@mui/material-nextjs/v13-pagesRouter";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
+import { LocalizationProvider } from "@mui/x-date-pickers";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AppCacheProvider {...pageProps}>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Component {...pageProps} />
+      </LocalizationProvider>
+    </AppCacheProvider>
+  );
 }
